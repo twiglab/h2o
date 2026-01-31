2f4254bea57e113e18b1f1eb89cc063f67b7eac6
it512 <it512@msn.com>
2026-01-31 13:32:44 +0800

    flag

--- hank/cmd/hank/main.go
+++ hank/cmd/hank/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"log/slog"
 	"net/http"
@@ -8,7 +10,24 @@ import (
 	hank "github.com/twiglab/h2o/octopus"
 )
 
+var (
+	borker     string
+	logPath    string
+	serverPath string
+)
+
+func init() {
+	flag.StringVar(&borker, "borker", "127.0.0.1:1088", "borker")
+	flag.StringVar(&logPath, "logpath", ".", "logpath")
+	flag.StringVar(&serverPath, "ctxpath", "/sync", "ctxpath")
+}
+
 func main() {
+	flag.Parse()
+
+	fmt.Println(borker)
+	fmt.Println(logPath)
+	fmt.Println(serverPath)
 
 	dl := hank.NewLog("data.log", slog.LevelDebug)
 	il := hank.NewLog("info.log", slog.LevelDebug)
@@ -19,7 +38,7 @@ func main() {
 	}
 	h := hank.Handle(hub)
 
-	http.HandleFunc("/sync", h)
+	http.HandleFunc(serverPath, h)
 
 	if err := http.ListenAndServe("0.0.0.0:20000", nil); err != nil {
 		log.Fatal(err)
--- hank/go.mod
+++ hank/go.mod
@@ -9,15 +9,8 @@ require (
 )
 
 require (
-	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
-	github.com/philhofer/fwd v1.2.0 // indirect
-	github.com/tinylib/msgp v1.6.1 // indirect
-	golang.org/x/mod v0.31.0 // indirect
 	golang.org/x/net v0.48.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
-	golang.org/x/tools v0.40.0 // indirect
 )
-
-tool github.com/tinylib/msgp
