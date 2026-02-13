2fe4f1a14966809e8693ff574add95aba2023c0f
it512 <it512@msn.com>
2026-02-13 20:58:28 +0800

    cmd

---
+++ hank/cmd/hank/cmd/run.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/twiglab/h2o/hank"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// runCmd represents the run command
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return run()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(runCmd)
+}
+
+func run() error {
+
+	s := &hank.Server{
+		Addr: viper.GetString("server.addr"),
+		Hub:  &hank.Hub{},
+	}
+
+	go http.ListenAndServe(viper.GetString("web.addr"), nil)
+
+	return s.Run()
+}
