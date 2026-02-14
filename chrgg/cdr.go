7066cefb79f2a10d48a37cafb4f9bc010baa77ca
it512 <it512@msn.com>
2026-02-14 17:52:28 +0800

    cdr

--- chrgg/cdr.go
+++ chrgg/cdr.go
@@ -3,17 +3,22 @@ package chrgg
 import "time"
 
 type CDR struct {
+	DeviceCode string
+	Type       string
+
 	DataCode     string
 	LastDataCode string
 
 	DataTime     time.Time
 	LastDataTime time.Time
 
-	DataValue     int64
-	LastDataValue int64
+	DataValue     int64 // 当前表显
+	LastDataValue int64 // 上次表显
 
 	PloyID string
 	RuleID string
+
+	Value int64 // 两次表显的差值, 用于计算费用的数值
 
 	Amount int64
 	Unit   int64
