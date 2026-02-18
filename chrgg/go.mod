d3c116f1bef819183b5aa86d2ae347e83f67f351
it512 <it512@msn.com>
2026-02-19 07:21:39 +0800

    pgx

--- chrgg/go.mod
+++ chrgg/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/eclipse/paho.mqtt.golang v1.5.1
 	github.com/go-sql-driver/mysql v1.9.3
 	github.com/google/uuid v1.6.0
+	github.com/jackc/pgx/v5 v5.8.0
 	github.com/mattn/go-sqlite3 v1.14.34
 	github.com/spf13/cobra v1.10.2
 	github.com/spf13/viper v1.21.0
@@ -30,6 +31,9 @@ require (
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/hashicorp/hcl/v2 v2.18.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
--- chrgg/orm/db.go
+++ chrgg/orm/db.go
@@ -1,14 +1,33 @@
 package orm
 
 import (
+	"context"
+
+	"entgo.io/ent/dialect"
+	entsql "entgo.io/ent/dialect/sql"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
+
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/twiglab/h2o/chrgg/orm/ent"
+
 	_ "github.com/twiglab/h2o/chrgg/orm/ent/runtime"
+
+	"github.com/twiglab/h2o/chrgg/orm/ent"
 )
 
 //go:generate go tool ent generate ./schema --target ./ent
 
 func OpenEntClient(name, dns string) (*ent.Client, error) {
+	if name == "pgx" {
+		pool, err := pgxpool.New(context.Background(), dns)
+		if err != nil {
+			return nil, err
+		}
+		db := stdlib.OpenDBFromPool(pool)
+		drv := entsql.OpenDB(dialect.Postgres, db)
+		return ent.NewClient(ent.Driver(drv)), nil
+	}
+
 	return ent.Open(name, dns)
 }
