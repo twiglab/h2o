0d9e268ebbada137ff47a94ea02a57b448e26e30
it512 <it512@msn.com>
2026-02-23 09:25:21 +0800

    remove nh-fe

--- fe/frontend/src/stores/index.ts
+++
@@ -1,4 +0,0 @@
-export { useUserStore } from './modules/user'
-export { usePermissionStore } from './modules/permission'
-export { useAppStore } from './modules/app'
-export { useTagsViewStore } from './modules/tagsView'
--- fe/services/shared/config/jwt.go
+++
@@ -1,8 +0,0 @@
-package config
-
-// JWTConfig JWT认证配置
-type JWTConfig struct {
-	Secret           string `mapstructure:"secret"`
-	AccessExpireMin  int    `mapstructure:"access_expire_min"`
-	RefreshExpireDay int    `mapstructure:"refresh_expire_day"`
-}
