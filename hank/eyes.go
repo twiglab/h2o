00fe071831c8b9d5589fcac6215b26e4c9128174
it512 <it512@msn.com>
2026-02-26 17:06:49 +0800

    add windows func

--- hank/eyes.go
+++ hank/eyes.go
@@ -5,13 +5,52 @@ import (
 	"net/http"
 	"slices"
 	"time"
+
+	"github.com/twiglab/h2o/pkg/common"
 )
+
+type window struct {
+	candidate []common.Electricity
+	pos       int
+	l         int
+}
+
+func newWin(l int) *window {
+	return &window{
+		candidate: make([]common.Electricity, l, l),
+		l:         l,
+	}
+}
+
+func (w *window) Add(ele common.Electricity) {
+	np := w.pos % w.l
+	w.candidate[np] = ele
+	w.pos = np + 1
+}
 
 type ElectricityItem struct {
 	ElectricityMeter
-	AcctpeTime time.Time
+	AcceptTime time.Time     `json:"accept_time"`
 	Delay      time.Duration `json:"delay,format:sec"`
 	TimeOut    time.Duration `json:"timeout,format:sec"`
+
+	window *window `json:"-"`
+}
+
+func (ei *ElectricityItem) fill() {
+	for _, ele := range ei.window.candidate {
+		ei.Data.DataValue = max(ei.Data.DataValue, ele.DataValue)
+
+		ei.Data.VoltageA = max(ei.Data.VoltageA, ele.VoltageA)
+		ei.Data.VoltageB = max(ei.Data.VoltageB, ele.VoltageB)
+		ei.Data.VoltageC = max(ei.Data.VoltageC, ele.VoltageC)
+
+		ei.Data.CurrentA = max(ei.Data.CurrentA, ele.CurrentA)
+		ei.Data.CurrentB = max(ei.Data.CurrentB, ele.CurrentB)
+		ei.Data.CurrentC = max(ei.Data.CurrentC, ele.CurrentC)
+
+		ei.Data.TotalActivePower = max(ei.Data.TotalActivePower, ele.TotalActivePower)
+	}
 }
 
 type ElectricityPacket struct {
@@ -31,28 +70,26 @@ func (e *ElectricityPacket) Merge(m ElectricityMeter) {
 	)
 
 	if i, ok = e.Items[m.Code]; !ok {
-		i = &ElectricityItem{ElectricityMeter: m, AcctpeTime: time.Now()}
+		i = &ElectricityItem{ElectricityMeter: m, AcceptTime: time.Now(), window: newWin(10)}
+		i.window.Add(m.Data)
 		e.Items[i.Code] = i
 		return
 	}
+
+	i.window.Add(m.Data)
 
 	i.TimeOut = 0
 
 	i.Device = m.Device
 	i.Pos = m.Pos
-	i.Data = m.Data
 
-	i.AcctpeTime = time.Now()
+	i.AcceptTime = time.Now()
 	i.Delay = time.Since(m.DataTime)
-
-	if m.Data.DataValue > 0 {
-		i.Data.DataValue = m.Data.DataValue
-	}
-
 }
 
 func (e *ElectricityPacket) List() (items []*ElectricityItem) {
 	for _, item := range e.Items {
+		item.fill()
 		item.TimeOut = time.Since(item.DataTime)
 		items = append(items, item)
 	}
