96ec48910ab921ef191cca74702a5b84486e180c
it512 <it512@msn.com>
2026-02-28 15:34:42 +0800

    补充计费逻辑注释

--- chrgg/cdr.go
+++ chrgg/cdr.go
@@ -37,7 +37,7 @@ type CDR struct {
 }
 
 func CalcCDR(last LastCDR, cd ChargeData, cr ChargeRuler) CDR {
-	value, fee := calc(last.DataValue, cd.Data.DataValue, cr.UnitFeeFen())
+	value, fee := calcFee(last.DataValue, cd.Data.DataValue, cr.UnitFeeFen())
 	return CDR{
 		DeviceCode: cd.Code,
 		DeviceType: cd.Type,
@@ -66,8 +66,15 @@ func CalcCDR(last LastCDR, cd ChargeData, cr ChargeRuler) CDR {
 	}
 }
 
-func calc(lastV, currV int64, unit int64) (v int64, f int64) {
-	v = currV - lastV
-	f = v * unit / 100
+func calcFee(lastV, currV int64, unitFen int64) (
+	valuePer100 int64, /* 表差 1/100 计量单位*/
+	feeFen int64, /* 1 计量单位多少分 */
+) {
+	// 注意这里的表差是 1/100 计量单位
+	// 为了规避误差，这里先做乘法再做除法
+	// 最后除以100是把 1/100 计量单位，转成 1 计量单位
+	// 不是分转元! 所以最后得出的结果是 1 个计量单位多少分
+	valuePer100 = currV - lastV
+	feeFen = (valuePer100 * unitFen) / 100
 	return
 }
