90dc208ab5d511b7b85f8b4c2cc9f64c9772a836
it512 <it512@msn.com>
2026-03-03 09:05:37 +0800

    wal 增加topic

--- hank/hub.go
+++ hank/hub.go
@@ -19,7 +19,10 @@ func (h *Hub) HandleDeviceStatus(ctx context.Context, data DeviceStatus) error {
 }
 
 func (h *Hub) HandleElectricity(ctx context.Context, data ElectricityMeter) error {
-	h.WAL.WriteLogContext(ctx, wal.String("type", data.Type), wal.Any("data", data))
+	h.WAL.WriteLogContext(ctx,
+		wal.String("type", data.Type),
+		wal.Any("data", data),
+		wal.String("topic", data.Topic()))
 
 	h.EP.Merge(data)
 
@@ -27,7 +30,10 @@ func (h *Hub) HandleElectricity(ctx context.Context, data ElectricityMeter) erro
 }
 
 func (h *Hub) HandleWater(ctx context.Context, data WaterMeter) error {
-	h.WAL.WriteLogContext(ctx, wal.String("type", data.Type), wal.Any("data", data))
+	h.WAL.WriteLogContext(ctx,
+		wal.String("type", data.Type),
+		wal.Any("data", data),
+		wal.String("topic", data.Topic()))
 
 	return h.Sender.SendData(ctx, data)
 }
