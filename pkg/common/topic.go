b8652a7e2af9c44c1faf284865cdba044af30f13
it512 <it512@msn.com>
2026-03-03 22:37:12 +0800

    增加topic类型判断

--- pkg/common/topic.go
+++ pkg/common/topic.go
@@ -1,5 +1,7 @@
 package common
 
+import "strings"
+
 func Topic(d Device) string {
 	return "h2o/" + d.Code + "/" + d.Type
 }
@@ -9,3 +11,16 @@ const (
 	ElectricityTopic = "h2o/+/E"
 	GasTopic         = "h2o/+/G"
 )
+
+func TopicType(topic string) string {
+	t, _ := strings.CutSuffix(topic, "/")
+	switch t {
+	case WATER:
+		return WaterTopic
+	case ELECTRICITY:
+		return ElectricityTopic
+	case GAS:
+		return GasTopic
+	}
+	panic(topic + " not supports")
+}
