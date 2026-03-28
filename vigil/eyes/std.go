8c246753c207a5032cbe2ebfd45de39c2287e3bb
it512 <it512@msn.com>
2026-03-28 22:48:36 +0800

    方差

---
+++ vigil/eyes/std.go
@@ -0,0 +1,26 @@
+package eyes
+
+import (
+	"math"
+)
+
+func mean(v []float64) float64 {
+	var res float64
+	for _, x := range v {
+		res += x
+	}
+	return res / float64(len(v))
+}
+
+func variance(v []float64) float64 {
+	var res float64
+	var m = mean(v)
+	for _, x := range v {
+		res += (x - m) * (x - m)
+	}
+	return res / float64(len(v))
+}
+
+func std(v []float64) float64 {
+	return math.Sqrt(variance(v))
+}
