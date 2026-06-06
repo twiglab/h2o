7ee5e7025369d6d121d1d9d9db54b40e68ef1604
it512 <it512@msn.com>
2026-06-06 11:01:51 +0800

    代码整理

--- hank/json.go
+++ hank/json.go
@@ -9,20 +9,15 @@ func unmarshal(in []byte, out any) error {
 	return json.Unmarshal(in, out)
 }
 
-func marshalWrite(out io.Writer, in any) error {
-	return json.MarshalWrite(out, in)
-}
-
 func marshal(in any) ([]byte, error) {
 	return json.Marshal(in)
 }
 
 var newline = []byte{'\n'}
 
-func writeReturn(out io.Writer, in any) error {
-	if err := json.MarshalWrite(out, in); err != nil {
-		return err
+func writeReturn(out io.Writer, in any) (err error) {
+	if err = json.MarshalWrite(out, in); err == nil {
+		_, err = out.Write(newline)
 	}
-	_, err := out.Write(newline)
-	return err
+	return
 }
