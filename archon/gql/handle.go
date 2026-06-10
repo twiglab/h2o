463961555101bccd40994f4ee3f3aebb48291723
it512 <it512@msn.com>
2026-06-10 21:20:36 +0800

    gql handle

---
+++ archon/gql/handle.go
@@ -0,0 +1,23 @@
+package gql
+
+import (
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/twiglab/h2o/archon/gql/graph"
+	"github.com/vektah/gqlparser/v2/ast"
+)
+
+func Server() http.Handler {
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+	srv.Use(extension.Introspection{})
+	return srv
+}
