e384209ad991f57c8492a734316744610c3e4f44
it512 <it512@msn.com>
2026-07-23 17:58:16 +0800

    回复wal

--- hank/hub.go
+++ hank/hub.go
@@ -3,7 +3,8 @@ package hank
 import (
 	"context"
 	"encoding"
-	// "github.com/twiglab/h2o/clog/wal"
+
+	"github.com/twiglab/h2o/clog/wal"
 )
 
 type SendObject interface {
@@ -16,7 +17,7 @@ type Sender interface {
 }
 
 type Hub struct {
-	// WAL    *wal.WAL
+	WAL    *wal.WAL
 	Sender Sender
 }
 
@@ -25,23 +26,19 @@ func (h *Hub) HandleDeviceStatus(ctx context.Context, data DeviceStatus) error {
 }
 
 func (h *Hub) HandleElectricity(ctx context.Context, data ElectricityMeter) error {
-	/*
-		h.WAL.WriteLogContext(ctx,
-			wal.String("type", data.Type),
-			wal.Any("data", data),
-			wal.String("topic", data.Topic()))
-	*/
+	h.WAL.WriteLogContext(ctx,
+		wal.String("type", data.Type),
+		wal.Any("data", data),
+		wal.String("topic", data.Topic()))
 
 	return h.Sender.SendData(ctx, data)
 }
 
 func (h *Hub) HandleWater(ctx context.Context, data WaterMeter) error {
-	/*
-		h.WAL.WriteLogContext(ctx,
-			wal.String("type", data.Type),
-			wal.Any("data", data),
-			wal.String("topic", data.Topic()))
-	*/
+	h.WAL.WriteLogContext(ctx,
+		wal.String("type", data.Type),
+		wal.Any("data", data),
+		wal.String("topic", data.Topic()))
 
 	return h.Sender.SendData(ctx, data)
 }
