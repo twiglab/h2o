20035d35ad1f08a09490e113316e0bb9e4bced8d
it512 <it512@msn.com>
2026-08-22 15:43:24 +0800

    util

---
+++ vigil/orm/schema/util.go
@@ -0,0 +1,28 @@
+package schema
+
+import (
+	"fmt"
+	"uuid"
+
+	"entgo.io/ent/dialect"
+)
+
+func cdrid() string {
+	u := uuid.NewV7()
+	return u.String()
+}
+
+func char(size int) map[string]string {
+	return map[string]string{
+		dialect.MySQL:    fmt.Sprintf("char(%d)", size),
+		dialect.SQLite:   fmt.Sprintf("char(%d)", size),
+		dialect.Postgres: fmt.Sprintf("char(%d)", size),
+	}
+}
+func varchar(size int) map[string]string {
+	return map[string]string{
+		dialect.MySQL:    fmt.Sprintf("varchar(%d)", size),
+		dialect.SQLite:   fmt.Sprintf("varchar(%d)", size),
+		dialect.Postgres: fmt.Sprintf("varchar(%d)", size),
+	}
+}
