6df85d98ff0524ed56321a3581981b29cb91f513
it512 <it512@msn.com>
2026-09-04 06:14:22 +0800

    del

--- chrgg/meter.go
+++ chrgg/meter.go
@@ -8,27 +8,10 @@ import (
 
 type ElectyMeterData struct {
 	common.Device
-	Pos   common.Pos              `json:"pos,omitzero"`
-	Data  common.MeterValue       `json:"data"`
-	Param common.ElectricityParam `json:"param"`
-	Flag  common.Flag             `json:"flag,omitzero"`
-
-	Topic string `json:"topic"`
+	Data  common.MeterValue `json:"data"`
+	Topic string            `json:"topic"`
 }
 
 func (d *ElectyMeterData) UnmarshalBinary(data []byte) error {
-	return json.Unmarshal(data, d)
-}
-
-type WaterMeterData struct {
-	common.Device
-	Pos  common.Pos        `json:"pos,omitzero"`
-	Data common.MeterValue `json:"data"`
-	Flag common.Flag       `json:"flag,omitzero"`
-
-	Topic string `json:"topic"`
-}
-
-func (d *WaterMeterData) UnmarshalBinary(data []byte) error {
 	return json.Unmarshal(data, d)
 }
