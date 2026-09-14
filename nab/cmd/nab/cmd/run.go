5559c06f6ce045e4d45a4679923a631480432534
it512 <it512@msn.com>
2026-09-14 10:18:27 +0800

    x

--- nab/cmd/nab/cmd/run.go
+++ nab/cmd/nab/cmd/run.go
@@ -1,15 +1,8 @@
 package cmd
 
 import (
-	"log"
-	"os"
-
 	"net/http"
 	_ "net/http/pprof"
-
-	"github.com/twiglab/h2o/nab/box/driverbox"
-	"github.com/twiglab/h2o/nab/box/exports"
-	"github.com/twiglab/h2o/nab/box/plugins"
 
 	"github.com/spf13/cobra"
 )
@@ -34,38 +27,5 @@ func init() {
 }
 
 func run() error {
-	// 设置日志级别
-	_ = os.Setenv("LOG_LEVEL", "debug")
-	// 第一步: 启用内置Plugin
-	// 可选: plugins.EnableAll() 启用所有内置Plugin
-	// 或单独启用: 导入对应Plugin包,如 modbus.EnablePlugin()
-
-	plugins.EnableAll()
-
-	//modbus.EnablePlugin()
-	//httpserver.EnablePlugin()
-
-	// 第二步: 启用Export模块
-	// 可选: exports.EnableAll() 启用所有内置Export模块
-	// 或单独启用: 导入对应Export包,如 gateway.EnableExport()
-
-	exports.EnableAll()
-	// mirror.EnableExport()
-	//mqtt.EnablePlugin()
-
-	// 第三步: 启动 driver-box 服务
-	// 1. 初始化环境配置
-	// 2. 初始化日志记录器
-	// 3. 启动所有 Export
-	// 4. 启动所有 Plugin
-	// 5. 触发服务状态事件
-	err := driverbox.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer driverbox.Stop()
-	// 第四步: 阻塞主线程
-	// driver-box 服务会在后台运行
 	return http.ListenAndServe(":10001", nil)
 }
