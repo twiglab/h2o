4869563a67137e9db6ec95c3ce2f7e03af4629e9
it512 <it512@msn.com>
2026-09-21 18:19:16 +0800

    fake device

---
+++ nab/equlib/fake/fake.go
@@ -0,0 +1,57 @@
+package fake
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"github.com/simonvetter/modbus"
+	"github.com/twiglab/h2o/nab"
+	"github.com/twiglab/h2o/nab/equlib"
+	"github.com/twiglab/h2o/pkg/common"
+)
+
+func init() {
+	equlib.Register(FAKE_DEVIVE, &Fake{})
+}
+
+const FAKE_DEVIVE = "fake-device"
+
+type Fake struct {
+	optStatus int64
+}
+
+func (e Fake) Collect(ctx context.Context, _ *modbus.ModbusClient, data nab.DeviceData) error {
+
+	now := time.Now()
+
+	var meter nab.Meter
+	meter.Data.DataValue = now.Unix()
+	meter.Data.OptStatus = e.optStatus
+
+	meter.Code = data.Record.Code
+	meter.Type = data.Record.Typ
+	meter.DataCode = common.NewDataCode()
+	meter.DataTime = now
+	meter.DataTs = common.Ts(now)
+
+	meter.Pos.Project = data.Global.Project
+
+	meter.Gateway.Code = data.Global.Box
+	meter.Gateway.Type = common.GATEWAY_NH
+	meter.Gateway.UnitID = data.Record.UnitID
+
+	return data.Sender.SendData(ctx, meter)
+}
+
+func (e *Fake) On(ctx context.Context, cli *modbus.ModbusClient, data nab.DeviceData) error {
+	data.Logger.DebugContext(ctx, "FAKE DEVICE ON", slog.String("boxCode", data.Global.Box), slog.Any("Record", data.Record))
+	e.optStatus = common.OPT_STATUS_ON
+	return nil
+}
+
+func (e *Fake) Off(ctx context.Context, cli *modbus.ModbusClient, data nab.DeviceData) error {
+	data.Logger.DebugContext(ctx, "FAKE DEVICE OFF", slog.String("boxCode", data.Global.Box), slog.Any("Record", data.Record))
+	e.optStatus = common.OPT_STATUS_OFF
+	return nil
+}
